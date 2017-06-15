3f06f7bab9e7a92fd2d0f683925eb29abe04323e
ARLineAR <alinerambier@gmail.com>
2017-06-15 16:34:36 +0200

    Modif UsersController.php

--- silex-skeleton/src/Controller/Admin/UsersController.php
+++ silex-skeleton/src/Controller/Admin/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace Controller\Admin;
 
-class MembreController {
+class userController {
     
     public function listAction(){
         
@@ -10,8 +10,8 @@ class MembreController {
         
         return $this->render(
                 
-            'admin/membre/list.html.twig',
-            ['user'=> $users]
+            'admin/user/list.html.twig',
+            ['users'=> $users]
         );
     }
     
@@ -22,7 +22,7 @@ class MembreController {
             $user= $this->app['user.repository']->find($id);
         }else{
             
-            $user = new Membre;
+            $user = new User;
         }
         
         if(empty($_POST)){
@@ -32,25 +32,25 @@ class MembreController {
                     ->setLastName($_POST['lastname'])
                     ->setFirstName($_POST['firstname'])
                     ->setAdress($_POST['adress'])
-                    ->setZipCode($_POST['zip_code'])
+                    ->setZipcode($_POST['zipcode'])
                     ->setCity($_POST['city'])
-                    ->setTelephone($_POST['telephone'])
-                    ->setStatuts($_POST['status']);
+                    ->setPhonenumber($_POST['phonenumber'])
+                    ->setStatus($_POST['status']);
             
             $user = $this->app['user.repository']->save($article);
-            $this->addFlashMessage('Le membre a bien été enregistré');
+            $this->addFlashMessage("L'utilisateur a bien été modifié");
         }
         
     }
     
     public function deleteAction($id){
         
-        $user = $this->app['membre.repository']->finc($id);
+        $user = $this->app['user.repository']->find($id);
         
-        $this->app['membre.repository']->delete($user);
-        $this->addFlashMessage('Le membre a bien été supprimé');
+        $this->app['user.repository']->delete($user);
+        $this->addFlashMessage("L'utilisateur a bien été supprimé");
         
-        return $this->redirectRoute('admin_membres');
+        return $this->redirectRoute('admin_users');
     }
     
    
