9bc17ad2e821f9a0e1731bf1fa402bb66adbbba4
TanguyPi <tanguy.pichavant@hotmail.com>
2017-06-15 16:04:33 +0200

    Auto stash before merge of "membres" and "origin/membres"

---
+++ silex-skeleton/src/Controller/ControllerAbstract.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Validator\RecursiveValidator;
+use Twig_Environment;
+
+abstract class ControllerAbstract {
+
+
+        /**
+     *
+     * @var Application2
+     */
+    protected $app;
+    
+    /**
+     *
+     * @var Twig_Environment
+     */
+    
+    protected $twig;
+    
+    
+    /**
+     *
+     * @var Session
+     */
+    protected $session;
+    /**
+     * 
+     * @param Application2 $app
+     */
+    
+    /**
+     *
+     * @var RecursiveValidator
+     */
+    protected $validator;
+
+    public function __construct(Application $app){
+       
+        $this->app = $app;
+        $this->twig = $app['twig'];
+        $this->session =$app['session'];
+        $this->validator =$app['validator'];
+         
+    }
+
+
+      /**
+     * 
+     * @param string $view
+     * @param array $parameters
+     * @return string
+     */
+    
+    public function render($view, $parameters =[]){
+        return $this->twig->render($view, $parameters);
+        
+    }
+    
+    /**
+     * Redirige vers une autre page en lui passant le nom de la route
+     * 
+     * @param type $routeName
+     * @param array $parameters
+     * @return Response
+     */
+
+      public function redirectRoute($routeName, array $parameters = []){
+        return $this->app->redirect(
+            $this->app['url_generator']->generate($routeName, $parameters)
+        ); 
+
+    }
+
+    public function addflashMessage($message, $type = 'success'){
+        $this->session->getFlashBag()->add($type, $message); 
+    }
+    
+  /**
+   * 
+   * @param mixed $value
+   * @param Constraint $constraint
+   * @return bool
+   */
+    public function validate($value, Constraint $constraint) {
+        
+        $errors = $this->validator->validate($value, $constraint);
+        
+        // s'il est vide; c'est que la valeur est valide
+        return $errors->count() == 0;
+        
+    }
+}
