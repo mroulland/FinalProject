b47593ec4e516451faed74f5a9b8f840689e67ff
TanguyPi <tanguy.pichavant@hotmail.com>
2017-06-15 14:17:17 +0200

    User.php (done)

--- silex-skeleton/src/Entity/User.php
+++ silex-skeleton/src/Entity/User.php
@@ -6,23 +6,37 @@ class User {
 
     private $id;
 
-    private $lastname;
-
-    private $name;
-
-    private $phone;
-
     private $email;
 
     private $password;
 
-    private $status;
+    private $lastname;
+
+    private $name;
+
+    private $adresse;
+
+    private $code_postal;
+
+    private $ville;
+
+    private $telephone;
+
+    private $statut;
 
     
 
-    //Getter-Setter
+    //GETTER
     public function getId() {
         return $this->id;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
     }
 
     public function getLastname() {
@@ -33,22 +47,82 @@ class User {
         return $this->lastname;
     }
 
-     public function getPhone() {
-        return $this->email;
+     public function getAdresse() {
+        return $this->adresse;
     }
 
-     public function getEmail() {
-        return $this->email;
+    public function getCode_postal() {
+        return $this->code_postal;
     }
 
-     public function getPassword() {
-        return $this->password;
+    public function getVille() {
+        return $this->ville;
     }
 
-     public function getStatus() {
-        return $this->status;
+     public function getTelephone() {
+        return $this->telephone;
+    }
+
+     public function getStatut() {
+        return $this->statut;
     }
     
 
+
+      public function isAdmin() {
+        return $this->statut == 'admin';
+    }
+
+
+    //Setter
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setPassword($password){
+        $this->password=$password;
+        return $this;
+    }
+
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function setAdresse($adresse){
+        $this->adresse= $adresse;
+        return $this;
+    }
+
+    public function setCode_postal($code_postal){
+        $this->code_postal= $code_postal;
+        return $this;
+    }
+
+    public function setVille($ville){
+        $this->ville= $ville;
+        return $this;
+    }
+
+    public function setTelephone($telephone){
+        $this->telephone= $telephone;
+        return $this;
+    }
+
+    public function setstatut($statut){
+        $this->statut=$statut;
+        return $this;
+    }
 
 }
