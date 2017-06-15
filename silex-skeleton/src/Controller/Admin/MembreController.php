a078ee7a000f3ab28e0d859a0f778dde8c4f7f85
mroulland <morgane.roulland@hotmail.fr>
2017-06-15 11:19:35 +0200

    Edit MembreController

--- silex-skeleton/src/Controller/Admin/MembreController.php
+++ silex-skeleton/src/Controller/Admin/MembreController.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Controller\Admin;
 
-/**
- * Description of MembreController
- *
- * @author Etudiant
- */
 class MembreController {
-    //put your code here
+    
 }
