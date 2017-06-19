47ccfffc5e8761e872b9efe736555826c4a1d62f
RemiWa <remi.warin@gmail.com>
2017-06-19 16:13:36 +0200

    WIP SRC/Controller/admin/ProductController

--- silex-skeleton/src/Controller/Admin/ProductController.php
+++ silex-skeleton/src/Controller/Admin/ProductController.php
@@ -12,16 +12,50 @@ class ProductController extends ControllerAbstract {
         $products = $this->app['product.repository']->findAllProducts();
 
         return $this->render(
-            'product.html.twig',
+            'admin/user/product.html.twig',
             ['products' => $products]
         );
     }
 
-    public function editAction($id = null){
+        public function editAction($id_product = null){
 
+                if(!is_null($id_product)){
 
+                    $product= $this->app['product.repository']->find($id_product);
+                }else{ //Si non, création d'un nouveau membre
 
-    }
+                    $product = new Product;
+                }
 
+                if(empty($_POST)){ // création d'un nouvel utilsateur
+                    $product
+                            ->setIdProduct($_POST['id_product'])
+                            ->setProductName($_POST['product_name'])
+                            ->setDescription($_POST['description'])
+                            ->setPhoto($_POST['photo'])
+                            ->setPrice($_POST['price']);
+
+                    $this->app['product.repository']->save($product);
+                    $this->addFlashMessage("Le produit a bien été modifié");
+                    return $this->redirectRoute('admin_product');
+                }
+
+                return $this->render(
+                    'admin/product/edit.html.twwig',
+                    [
+                        'product' =>$product,
+                    ]
+                );
+            }
+
+        public function deleteAction($id_product){
+            $product = $this->app['product.repository']->find($id_product);
+
+            $this->app['product.repository']->delete($product);
+            $this->addflashMessage('le produit a été supprimé');
+
+            return $this->redirectRoute('admin_product');
+
+        }
 
 }
