844ca2549e4020eaeca8e7fcbf45c7aa2dfdd368
XavierLac <xavierlacombe67@gmail.com>
2017-06-19 11:32:18 +0200

    Nav admin

--- silex-skeleton/src/controllers.php
+++ silex-skeleton/src/controllers.php
@@ -34,6 +34,13 @@ $app
     // Nom de la route
     ->bind('register')
 ;
+
+$app->get('/layout', function () use ($app) {
+    return $app['twig']->render('layout.html.twig', array());
+})
+->bind('layout')
+;
+
 $app->get('/test', function () use ($app) {
     return $app['twig']->render('register.html.twig', array());
 })
