38474c0b29fb29372f5b97a6c118c80cd9d451c7
TanguyPi <tanguy.pichavant@hotmail.com>
2017-06-20 10:50:27 +0200

    Mail contact btoc btob + renvoie vers boite mail de la boite

---
+++ silex-skeleton/templates/formmail2.php
@@ -0,0 +1,18 @@
+<?php
+
+//Renvoie du mail de contact pour l'offre BtoB vers le mail des fleursdici (pour le moment mon mail à changer)
+//Il faut tester en ligne et non en local !
+$T0 = "tanguy.pichavant@hotmail.com";
+$h = "From: " . $TO; 
+
+$message = ""; 
+
+while (list($key, $val) = each($HTTP_POST_VARS)) { 
+$message .= "$key : $val\n"; 
+} 
+
+mail($TO, $subject, $message, $h);
+
+Header("Location: index.html"); 
+
+?>
