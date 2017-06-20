3dad0e41d50d5f76c0e2575ebbed001c3e1816d4
TanguyPi <tanguy.pichavant@hotmail.com>
2017-06-20 12:15:32 +0200

    Entreprisecontorller + commandecontroller + forms

---
+++ silex-skeleton/src/Controller/EntrepriseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Controller;
+
+//USE ??
+
+class EntrepriseController extends ControllerAbstract{
+
+    public function sendMessage(){
+
+        //Validation des champs du formulaire de contact BtoB
+        if(!empty ($_POST)){
+
+             if (!$this->validate($_POST['nameentreprise'], new Assert\NotBlank())){               
+
+                $errors['nameentreprise'] = 'Le nom de l\'entreprise est obligatoire';
+             }
+
+            if (!$this->validate($_POST['email'], new Assert\NotBlank())){ 
+                $errors['email'] = 'L\'email obligatoire';
+
+            } elseif(!$this->validate($_POST['email'], new Assert\Email())){
+
+                $errors['email'] = "L'email n'est pas valide";
+            }
+            
+            if(!$this->validate($_POST['phone'], new Assert\NotBlank())){
+
+                $errors['phone'] = 'Telephone n\'est pas valide';
+            }
+
+            if(!$this->validate($_POST['zipcode'], new Assert\NotBlank())){
+
+                $errors['zipcode'] = 'Le code postal n\'est pas valide';
+            }
+
+            if(!$this->validate($_POST['message'], new Assert\NotBlank())){
+
+                $errors['message'] = 'Le message n\'est pas valide';
+            }
+
+        }
+
+    }
+
+
+
+
+
+}
