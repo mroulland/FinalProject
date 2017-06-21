3d72d03bd3c2524a672f95d1743f7517c8c094e8
ARLineAR <alinerambier@gmail.com>
2017-06-21 14:13:39 +0200

    MAJ merge

--- silex-skeleton/src/Controller/Admin/ShippingController.php
+++ silex-skeleton/src/Controller/Admin/ShippingController.php
@@ -1,24 +1,24 @@
-<?php
-
-// Gestion des livraisons en back office
-
-namespace Controller\Admin;
-
-use Controller\ControllerAbstract;
-use Repository\UserRepository;
-use Entity\User;
-use Symfony\Component\Validator\Constraints as Assert;
-
-
-    class ShippingController extends ControllerAbstract{
-
-        public function listAction(){
-
-        ship = $this->app['shipping.repository']->findAllShips();
-
-        return $this->render(
-            'admin/shipping/list.html.twig',
-            ['ships' => $ships]
-        );
-
+<?php
+
+// Gestion des livraisons en back office
+
+namespace Controller\Admin;
+
+use Controller\ControllerAbstract;
+use Repository\UserRepository;
+use Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+    class ShippingController extends ControllerAbstract{
+
+        public function listAction(){
+
+        ship = $this->app['shipping.repository']->findAllShips();
+
+        return $this->render(
+            'admin/shipping/list.html.twig',
+            ['ships' => $ships]
+        );
+
     }
