b715f7444bc58c57ec20731ab457de5ffd559b9a
TanguyPi <tanguy.pichavant@hotmail.com>
2017-06-23 13:36:35 +0200

    Auto stash before merge of "membres" and "origin/membres"

--- silex-skeleton/web/js/scriptproducer.js
+++ silex-skeleton/web/js/scriptproducer.js
@@ -3,7 +3,7 @@ function myMap() {
       var myLatLng = {lat: 48.4333, lng: 2.15};
     
       var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 3,
           center: myLatLng
         });
         var marker = new google.maps.Marker({
