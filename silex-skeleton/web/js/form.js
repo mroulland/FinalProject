2abe25712fb4ad4441f7d80f14412de7c7ca1070
mroulland <morgane.roulland@hotmail.fr>
2017-06-26 19:28:33 +0200

    MAJ PAIEMENT !!!

--- silex-skeleton/web/js/form.js
+++ silex-skeleton/web/js/form.js
@@ -1,27 +1,21 @@
-    Stripe.setPublishableKey('pk_test_EONcjbwaYwY6HwyzCP1D5xR6');
+Stripe.setPublishableKey('pk_test_EONcjbwaYwY6HwyzCP1D5xR6')
 
     var $form = $('#payment_form');
-    $form.submit(function (e) {
-
+    $form.submit(function (e){ 
         e.preventDefault();
-
+        
         $form.find('.button').attr('disabled', true);
-
+    
         Stripe.card.createToken($form, function(status, response){
 
             if(response.error){
-                              
-               $form.find('.message').remove();
-               $form.prepend('<div class="ui negative message>' + response.error.message + ' </div>');
-               
+                $form.find('.message').remove();
+                $form.prepend('<div class="ui negative message>' + response.error.message + ' </div>');
             } else {
-           
                 var token = response.id;
-                $form.append('<input type="hidden" name="stripeToken">')
-                
-           }
-           
-  
-    })
-    })
-    
+                $form.append($('<input type="hidden" name="stripeToken">').val(token));
+                $form.get(0).submit();
+            }
+            
+        });
+    });
