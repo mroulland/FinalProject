ad67510c59a28c7dc493f5bba3cdedcb3832fdf8
ARLineAR <alinerambier@gmail.com>
2017-06-27 20:11:20 +0200

    Tentative MAJ subscription JS

--- silex-skeleton/web/js/scriptsubscription.js
+++ silex-skeleton/web/js/scriptsubscription.js
@@ -1,23 +1,39 @@
 // Fonction d'attente de chargement du DOM
 
 $(document).ready(function(){
+    
+    
+    
+    $('#selectproduit2').change(function(){
+         
+                // Modifier l'image au choix de l'utilisateur
+                if($(this).val() === 'moyen'){
+                    $('img').attr('src', 'img/produit1.jpg');
 
-    $('#radiomode2').click(function() {
-    $('#mappointrelais').slideDown('slow', function() {
-        // Animation complete.
+                }else{
+                    $('img').attr('src', 'img/produit2.jpg');
+
+                };
+        });
+    
+  
+
+        $('#radiomode2').click(function() {
+        $('#mappointrelais').slideDown('slow', function() {
+            // Animation complete.
+        });
     });
-});
 
-    $('#radiomode1').click(function() {
-    $('#mappointrelais').slideUp('slow', function(){
+        $('#radiomode1').click(function() {
+       $('#mappointrelais').slideUp('slow', function(){
 
+        });
     });
-});
 
 
-   $("#selectproduit2").change(function(){
-     $("img[name=image-swap]").attr("src",$(this).val());
+   //$("#selectproduit2").change(function(){
+     //$("img[name=image-swap]").attr("src",$(this).val());
 
-   });
+   //});
 
 });
