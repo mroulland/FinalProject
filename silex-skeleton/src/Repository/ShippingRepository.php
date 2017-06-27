5ddffc6900dfc3d4fe0adf1dc3659bbc36678905
mroulland <morgane.roulland@hotmail.fr>
2017-06-27 16:30:13 +0200

    Maj diverses et variées

--- silex-skeleton/src/Repository/ShippingRepository.php
+++ silex-skeleton/src/Repository/ShippingRepository.php
@@ -1,14 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Repository;
 
 use Entity\Shipping;
+use Repository\RepositoryAbstract;
 
 
 class ShippingRepository extends RepositoryAbstract {
