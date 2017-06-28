883cbe4c732a61c33a727a92fa3ef0e1e5e94476
RemiWa <remi.warin@gmail.com>
2017-06-28 12:57:31 +0200

    Auto stash before merge of "membres" and "origin/membres"

--- silex-skeleton/src/Controller/SubscriptionController.php
+++ silex-skeleton/src/Controller/SubscriptionController.php
@@ -11,72 +11,72 @@ use Controller\StripeController;
 use Service\UserManager;
 
 /*
-* ABONNEMENT 
+* ABONNEMENT
 */
 
 class SubscriptionController extends ControllerAbstract{
 
     // Affichage et traitement du formulaire d'abonnement
-    
+
     public function subscriptionAction($productId = null){
-        
-        
-        
+
+
+
         if(!empty($_POST)){
-            
-            // Vérification des champs du formulaire 
+
+            // Vérification des champs du formulaire
             if($_POST['frequency'] != "null" && $_POST['size'] != "null" && (!empty($_POST['mode']))){
                 // La fonction findChoosenProduct analyse les choix de l'utilisateur pour trouver le produit correspondant
-                $product = $this->app['product.repository']->findChoosenProduct($_POST['size'], $_POST['frequency']);   
+                $product = $this->app['product.repository']->findChoosenProduct($_POST['size'], $_POST['frequency']);
                 $shipping = $this->app['shipping.repository']->findById($_POST['mode']);
-                             
+
                 return $this->redirectRoute(
-                    'panier', 
+                    'panier',
                         [
                             'productId' => $product->getIdProduct(),
-                            'shippingId' => $shipping->getIdShipping()                     
-                        ]      
+                            'shippingId' => $shipping->getIdShipping()
+                        ]
                 );
             }
             else{
                 $msg = '<strong>Le formulaire contient des erreurs</strong>';
                 $this->addFlashMessage($msg, 'error');
-            }             
+            }
         }
         //intval pour transformer string en int
         return $this->render('subscription.html.twig',  ['productId' => $productId]
                 );
     }
-    
+
 /*
- * AFFICHAGE DU PANIER 
+ * AFFICHAGE DU PANIER
  */
     /**
      * Affiche le panier
      * @return string
-     * 
+     *
      */
     public function panierList($productId, $shippingId){
-        
+
             $product = $this->app['product.repository']->findById($productId);
             $shipping = $this->app['shipping.repository']->findById($shippingId);
-            
-    
+
+
         return $this->render(
-            'panier.html.twig', 
+            'panier.html.twig',
             [
                 'product' => $product,
                 'shipping' => $shipping
             ]
-                
+
         );
     }
-    
-    
 
-    
+
+
+
 /*
-* MODIFICATION ABONNEMENT 
+* MODIFICATION ABONNEMENT
 */
     public function editSubscription($id_subscription= null){
 
@@ -97,16 +97,16 @@ class SubscriptionController extends ControllerAbstract{
             $this->addFlashMessage('Modifications enregistrées');
             return $this->redirectRoute('profil');
             }
-        }     
+        }
 
 /*
 * SUSPENSION/REPRISE ABONNEMENT
-*/ 
+*/
     public function toggleSubscription($id = null){
 
         if(!is_null($id)){
             $subscription = $this->app['subscription.repository']->findByIdUser($id);
-            
+
             if($subscription->getSoftDelete() == '1'){
                 $subscription
                 ->setSoftDelete('0');
@@ -124,30 +124,30 @@ class SubscriptionController extends ControllerAbstract{
 
                 $this->addFlashMessage("L'abonnement a bien été réactivé");
             }
-            
+
             return $this->redirectRoute('profil');
         }
-    }        
-  
+    }
+
 
 /*
 * Création d'un utilisateur qui paye (customer)
 */
     /*public function createPaiement($productId, $shippingId){
-                 
+
         // Si l'utilisateur n'est pas connecté, on le redirige
-        
+
         if($this->app['user.manager']->isUserConnected()){
-            
+
             // On récupère un objet produit correspondant à celui du panier
             // On le stock en Session
-            $product = $this->app['product.repository']->findById($productId);         
+            $product = $this->app['product.repository']->findById($productId);
             $_SESSION['product'] = $product;
-            
+
             // Idem avec la livraison
-            $shipping = $this->app['shipping.repository']->findById($shippingId);           
+            $shipping = $this->app['shipping.repository']->findById($shippingId);
             $_SESSION['shipping'] = $shipping;
-                      
+
             $totalPrice = $product->getPrice() + $shipping->getShippingFees();
 
             if(!empty($_POST)){
@@ -167,7 +167,7 @@ class SubscriptionController extends ControllerAbstract{
                 if (!$this->validate($_POST['stripeToken'], new Assert\NotBlank())){
                         $errors['stripeToken'] = 'Token est obligatoire';
                 }
-                 
+
                 //Vérification des champs du form paiment:
                 if(!(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))){
                         $errors['email'] = 'L\'email obligatoire';
@@ -183,7 +183,7 @@ class SubscriptionController extends ControllerAbstract{
                 if (!$this->validate($_POST['monthcb'], new Assert\NotBlank())){
                         $errors['monthcb'] = 'Le mois est obligatoire';
                 }
-                
+
                 if (!$this->validate($_POST['yearcb'], new Assert\NotBlank())){
                         $errors['yearcb'] = 'L\'année estest obligatoire';
 
@@ -191,82 +191,82 @@ class SubscriptionController extends ControllerAbstract{
                 if (!$this->validate($_POST['cvc'], new Assert\NotBlank())){
                         $errors['cvc'] = 'Le cvc est obligatoire';
                 }
-                
+
                 // S'il n'y a pas d'erreur ...
                 if(empty($errors)){
-  
+
                     // On instancie une classe StripeController
                     $stripe= new StripeController ('sk_test_3JZ1xtsopRAl4LskpBAUKKFX');
-                    
+
                     // On crée un nouvel utilisateur Stripe
-                    $customer= $stripe->api('customers',[                
+                    $customer= $stripe->api('customers',[
                         'source' => $token,
                         'description' =>  $email,
-                    ]);                   
+                    ]);
 
                    //Création charges:
-                    $charge = $stripe->api('charges',[ 
-                             
+                    $charge = $stripe->api('charges',[
+
                         //En centimes!
-                        'amount'=> $totalPrice * 100,        
+                        'amount'=> $totalPrice * 100,
                         'currency' => 'eur',
                         'customer' => $customer->id,
-                    ]);  
-                    
+                    ]);
+
                     // On instancie une nouvelle classe Subscription
                     $subscription = new Subscription();
-                    
+
                     // On ajoute en BDD les informations concernant la commande
                     $subscription
-                        ->setIdUser($this->app['user.manager']->getUserId())             
+                        ->setIdUser($this->app['user.manager']->getUserId())
                         ->setIdProduct($_SESSION['product']->getIdProduct())
                         ->setIdShipping($_SESSION['shipping']->getIdShipping())
                         ->setStartDate($this->app['subscription.repository']->date())
                         ->setEndDate(NULL)
                     ;
                     // Insertion en BDD
-                    $this->app['subscription.repository']->insert($subscription);    
-                    
+                    $this->app['subscription.repository']->insert($subscription);
+
                     $this->addFlashMessage("Votre commande a bien été enregistrée");
-                    return $this->redirectRoute('profil');                    
+                    return $this->redirectRoute('profil');
                 }
-                
+
                 else{
                     $msg = '<strong>Le formulaire contient des erreurs</strong>';
                     $msg .='<br>- ' . implode('</br>- ', $errors);
 
                     $this->addFlashMessage($msg,'error');
                 }
-            }                  
+            }
     }
         else {
-    
+
             return $this->redirectRoute('login');
         }
-    return $this->render('paiement.html.twig'); 
+    return $this->render('paiement.html.twig');
 }*/
 
 
 
 public function createNewSubscription($productId, $shippingId){
-                 
+
         // Si l'utilisateur n'est pas connecté, on le redirige
-        
+
         if($this->app['user.manager']->isUserConnected()){
-            
+
             // On récupère un objet produit correspondant à celui du panier
             // On le stock en Session
-            $product = $this->app['product.repository']->findById($productId);         
+            $product = $this->app['product.repository']->findById($productId);
             $_SESSION['product'] = $product;
-            
+
             // Idem avec la livraison
-            $shipping = $this->app['shipping.repository']->findById($shippingId);           
+            $shipping = $this->app['shipping.repository']->findById($shippingId);
             $_SESSION['shipping'] = $shipping;
-                     
-            
+
+
             if($productId == 1 && $shippingId == 2){
                 $plan = '1';
-            } 
+            }
             elseif($productId == 1 && $shippingId == 1){
                 $plan = '2';
             }
@@ -288,8 +288,8 @@ public function createNewSubscription($productId, $shippingId){
             elseif($productId == 4 && $shippingId == 1){
                 $plan = '8';
             }
-            
-           
+
+
             if(!empty($_POST)){
 
                 $token = $_POST['stripeToken'];
@@ -307,7 +307,7 @@ public function createNewSubscription($productId, $shippingId){
                 if (!$this->validate($_POST['stripeToken'], new Assert\NotBlank())){
                         $errors['stripeToken'] = 'Token est obligatoire';
                 }
-                 
+
                 //Vérification des champs du form paiment:
                 if(!(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))){
                         $errors['email'] = 'L\'email obligatoire';
@@ -323,7 +323,7 @@ public function createNewSubscription($productId, $shippingId){
                 if (!$this->validate($_POST['monthcb'], new Assert\NotBlank())){
                         $errors['monthcb'] = 'Le mois est obligatoire';
                 }
-                
+
                 if (!$this->validate($_POST['yearcb'], new Assert\NotBlank())){
                         $errors['yearcb'] = 'L\'année est obligatoire';
 
@@ -331,7 +331,7 @@ public function createNewSubscription($productId, $shippingId){
                 if (!$this->validate($_POST['cvc'], new Assert\NotBlank())){
                         $errors['cvc'] = 'Le cvc est obligatoire';
                 }
-                
+
                 // S'il n'y a pas d'erreur ...
                 if(empty($errors)){
                     $user = $this->app['user.manager']->getUser();
@@ -339,18 +339,18 @@ public function createNewSubscription($productId, $shippingId){
                     $user->setStripeToken($token);
                     $this->app['user.repository']->update($user);
 
-                    
-                    
+
+
                     // On instancie une classe StripeController
                     $stripe = new StripeController ('sk_test_3JZ1xtsopRAl4LskpBAUKKFX');
-                    
+
                     // On crée un nouvel utilisateur Stripe
-                    $customer = $stripe->api('customers',[                
+                    $customer = $stripe->api('customers',[
                         'source' => $token,
-                        'description' => $lastname, 
-                        'email' => $email  
-                    ]);  
-                    
+                        'description' => $lastname,
+                        'email' => $email
+                    ]);
+
                     // CA MARCHE !!! var_dump($customer); die;
                     
                     $stripe->api("customers/{$customer->id}", [
@@ -360,59 +360,59 @@ public function createNewSubscription($productId, $shippingId){
                     
                     // On instancie une nouvelle classe Subscription
                     $subscription = new Subscription();
-                    
+
                     // On ajoute en BDD les informations concernant la commande
                     $subscription
-                        ->setIdUser($this->app['user.manager']->getUserId())             
+                        ->setIdUser($this->app['user.manager']->getUserId())
                         ->setIdProduct($_SESSION['product']->getIdProduct())
                         ->setIdShipping($_SESSION['shipping']->getIdShipping())
                         ->setStartDate($this->app['subscription.repository']->date())
                         ->setEndDate(NULL)
                     ;
                     // Insertion en BDD
-                    $this->app['subscription.repository']->insert($subscription);    
-                    
+                    $this->app['subscription.repository']->insert($subscription);
+
                     $this->addFlashMessage("Votre commande a bien été enregistrée");
-                    return $this->redirectRoute('profil');                    
+                    return $this->redirectRoute('profil');
                 }
-                
+
                 else{
                     $msg = '<strong>Le formulaire contient des erreurs</strong>';
                     $msg .='<br>- ' . implode('</br>- ', $errors);
 
                     $this->addFlashMessage($msg,'error');
                 }
-            }                  
+            }
     }
         else {
-    
+
             return $this->redirectRoute('login');
         }
-    return $this->render('paiement.html.twig'); 
+    return $this->render('paiement.html.twig');
 }
-     
 
 
 
 
 
-    
+
+
 /*
-* DESABONNEMENT 
+* DESABONNEMENT
 */
     public function deleteSubscription($id){
 
       $subscription = $this->app['subscription.repository']->findById($id);
-        
+
       $this->app['subscription.repository']->delete($subscription);
       $this->addFlashMessage("L'abonnement a bien été annulé");
-        
-      return $this->redirectRoute('profil');  
-     
+
+      return $this->redirectRoute('profil');
+
      }
 
 
-     
+
 
 
 }
