38b06f5658a28a93b90580aff64cb3c3ace4b8b0
mroulland <morgane.roulland@hotmail.fr>
2017-07-05 17:47:36 +0200

    Avancement carte cadeau 2%

--- silex-skeleton/src/Service/UserManager.php
+++ silex-skeleton/src/Service/UserManager.php
@@ -18,7 +18,6 @@ class UserManager{
         $this->session =$session;
     }
 
-
     /**
     *  Encodage:
     *
--- silex-skeleton/web/js/scriptsubscription.js
+++ silex-skeleton/web/js/scriptsubscription.js
@@ -1,34 +1,42 @@
 // Fonction d'attente de chargement du DOM
 
 $(document).ready(function(){
-    
-    
+
+    console.log("le dom est chargé");    
     
     $('#selectproduit2').change(function(){
          
-                // Modifier l'image au choix de l'utilisateur
-                if($(this).val() === 'moyen'){
-                    $('#photoproduit').attr('src', '../img/product1.jpg');
+        // Modifier l'image au choix de l'utilisateur
+        if($(this).val() === 'moyen'){
+            $('#photoproduit').attr('src', '../img/product1.jpg');
 
-                }else{
-                    $('#photoproduit').attr('src', '../img/product3.jpg');
+        }else{
+            $('#photoproduit').attr('src', '../img/product3.jpg');
 
-                };
-        });
+        };
+    });
     
-  
 
-        $('#radiomode2').click(function() {
+    $('#radiomode2').click(function() {
         $('#mappointrelais').slideDown('slow', function() {
             // Animation complete.
         });
     });
 
-        $('#radiomode1').click(function() {
-       $('#mappointrelais').slideUp('slow', function(){
+    $('#radiomode1').click(function() {
+        $('#mappointrelais').slideUp('slow', function(){
 
         });
     });
 
+    // Afficher le select durée pour offrir
+    $("#offrir").click(function(){
+        $('.timeselect').slideDown('slow');
+    });
+    
+    $("#abonnement").click(function(){
+        $('.timeselect').slideUp('slow');
+    });
+    
 
 });
