36e0415599c3fb5825c82bd748563bd4342bd6cf
ARLineAR <alinerambier@gmail.com>
2017-07-06 16:47:46 +0200

    MAJ LAYOUT + INDEX

--- silex-skeleton/web/js/layoutscript.js
+++ silex-skeleton/web/js/layoutscript.js
@@ -2,14 +2,14 @@
  * Quand l'utilisateur clique sur le bouton, le menu se déroule et se renroule 
 */
     function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById('myDropdown').classList.toggle('show');
     }
 
     // Cloture le menu si l'utilisateur clique ailleurs que sur le bouton
     window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
+      if (!event.target.matches.getElementById('#dropdown')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
         for (i = 0; i < dropdowns.length; i++) {
           var openDropdown = dropdowns[i];
@@ -19,3 +19,25 @@
         }
       }
     };
+    
+    
+    
+ $(document).ready(function(){
+     /* Gestion burger menu admin*/
+    $('#menuAdmin').click(function(evt) {
+           evt.preventDefault();
+           $('#navAdmin').slideToggle(function(){
+
+          });
+       });
+    
+    /* Gestion burger menu */
+    $('#menu').click(function(evt) {
+        evt.preventDefault();
+        $('#mainNav').slideToggle('slow',function(){
+
+       });
+    });
+ });
+    
+  
